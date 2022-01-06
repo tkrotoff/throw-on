994aceb0428518cf64bd17e38d1c9ed0201ce0b2
Tanguy Krotoff <tkrotoff@gmail.com>
2022-01-06 19:49:12 +0100

    Fix "ReferenceError: XMLHttpRequest is not defined"

--- src/throwOnXMLHttpRequestOpen.ts
+++ src/throwOnXMLHttpRequestOpen.ts
@@ -1,9 +1,18 @@
-const original = XMLHttpRequest.prototype.open;
+import assert from 'assert';
+
+// XMLHttpRequest is undefined under Node.js if not polyfilled
+// and thus accessing XMLHttpRequest.prototype.open fails when importing this file
+//const original = XMLHttpRequest.prototype.open;
+let original: typeof XMLHttpRequest.prototype.open | undefined;
 
 /**
  * Makes XMLHttpRequest.open to throw if called.
  */
 export function throwOnXMLHttpRequestOpen() {
+  if (original === undefined) {
+    original = XMLHttpRequest.prototype.open;
+  }
+
   XMLHttpRequest.prototype.open = (_method: string, url: string) => {
     throw new Error(`You must mock XMLHttpRequest: '${url}'`);
   };
@@ -13,5 +22,6 @@ export function throwOnXMLHttpRequestOpen() {
  * Restores the original XMLHttpRequest.open implementation.
  */
 export function restoreXMLHttpRequestOpen() {
+  assert(original !== undefined, 'Call throwOnXMLHttpRequestOpen first');
   XMLHttpRequest.prototype.open = original;
 }
